a02d8d16fa23dd9c73b0733f7598af5118c22485
sdh77 <141728845+sdh77@users.noreply.github.com>
2023-09-10 17:39:04 +0900

    매출전표
    정렬기능
    출력기능 완료

---
+++ admin/JS/order_list_script.js
@@ -0,0 +1,29 @@
+let selectYear = new Date().getFullYear();
+let selectMonth = String(new Date().getMonth()).padStart(2, "0");
+let selectDate = String(new Date().getDate()).padStart(2, "0");
+let date = `${selectYear}-${selectMonth}-${selectDate}`;
+let sort = "";
+
+const btn__order = document.querySelector(".toDayOrder__btn .btn__order");
+function changeSort() {
+  if (sort == "cnt") sort = "";
+  else sort = "cnt";
+  loadpageOrderList();
+}
+
+//로드 페이지
+function loadpageOrderList() {
+  let params = {
+    Date: date,
+    Sort: sort,
+  };
+
+  $.ajax({ url: "showtoDayOrderList.php", type: "get", data: params }).done(
+    function (data) {
+      $(".toDayOrder__list").html(data);
+    }
+  );
+}
+
+window.onload = loadpageOrderList();
+btn__order.addEventListener("click", changeSort);
---
+++ admin/showtoDayOrderList.php
@@ -0,0 +1,30 @@
+<?php
+$Date = $_GET['Date'];
+$Sort = $_GET['Sort'];
+$conn = pg_connect('host=localhost port=5432 dbname=ilprimo user=hanium_kioski password=aaa') or die('Could not connect: ' . pg_last_error());
+
+echo $Date;
+if($Sort == ""){
+  $sql = "select name, count(name)as cnt from orderdetail where DATE(date) = '".$Date."' group by name";
+}
+else{
+  $sql = "select name, count(name)as cnt from orderdetail where DATE(date) = '".$Date."' group by name order by " .$Sort;
+}
+
+$result = pg_query($conn, $sql);
+
+if ($result) {
+  $totalnum = pg_num_rows($result);
+  if ($totalnum > 0) {
+    while ($row = pg_fetch_assoc($result)) {
+      echo '<div class="toDayOrder__data"><div class="toDayOrder__name">'.$row["name"].'</div><div class="toDayOrder__cnt">'.$row["cnt"].'개</div></div>';  
+    }
+  }
+  else{
+    echo "<br>주문 내역 없음!!!";
+  }
+} else {
+  echo "오류 발생: " . pg_last_error($conn);
+}
+pg_close($conn);
+?>
