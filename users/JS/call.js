8c2db4c4b8b1162e76d096739cd12765250da74c
sdh77 <141728845+sdh77@users.noreply.github.com>
2023-09-18 13:02:31 +0900

    직원호출 클릭후 화면 종료..

--- users/JS/call.js
+++ users/JS/call.js
@@ -1,16 +1,16 @@
 const calls = document.querySelectorAll(".servText");
 
 calls.forEach(function (call) {
-  call.addEventListener("click",function(){
+  call.addEventListener("click", function () {
     callSend(call);
-  });  
+  });
 });
 
-function callSend(call){
+function callSend(call) {
   let name = call.innerHTML;
   let params = {
     Name: name,
   };
   $.ajax({ url: "callSend.php", type: "get", data: params });
-  .area-hidden
+  $(".serv-popup").removeClass("area-visible").addClass("area-hidden");
 }
