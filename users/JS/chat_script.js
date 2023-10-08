3d56e4e4a3eb7fe37826cd88f2bc2aeedd4ea9a4
ddyhye <ddy.hye@gmail.com>
2023-10-08 12:23:51 +0900

    챗봇 터치, 음성 모두 연결

--- users/JS/chat_script.js
+++ users/JS/chat_script.js
@@ -12,15 +12,95 @@ fetch("../config.json") // 설정 파일 로드
 $(document).ready(function () {
   // 직접 텍스트 입력했을 때 챗봇
   $(".chatdisplayArea-messageInput-sendBtn").click(function () {
-    let userMessage = $(".chatdisplayArea-messageInput-text").val();
+    let transcript = $(".chatdisplayArea-messageInput-text").val();
+    
+    flaskAjax(transcript);
+  });
 
+  // 음성으로 입력할 때 챗봇
+  if ("webkitSpeechRecognition" in window) {
+    const recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    let ListeningUserMessage = false;
+
+    recognition.onresult = function (event) {
+      console.log("음성 대기 중 ...");
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          const transcript = event.results[i][0].transcript.trim();
+          console.log(transcript);
+
+	  /*
+          if (transcript.includes("키오스키야") && !ListeningUserMessage) {
+            document.querySelector(".chatArea").style.display = "none";
+            document.querySelector(".chatdisplayArea").style.display = "block"; // jQuery 왜안됨
+
+            // 메시지를 chatbox에 추가
+            addMessageToChat(
+              "bot",
+              "어서오세요! 주문을 도와드리는 키오스키입니다^_^"
+            );
+            addMessageToChat("selector", "1. 메뉴 검색해줘");
+            addMessageToChat("selector", "2. 추천메뉴 알려줘");
+            addMessageToChat("selector", "3. 취향대로 추천해줘");
+            addMessageToChat("selector", "4. 주문 도와줘");
+            addMessageToChat("selector", "5. 직원 호출해 줘");
+
+            ListeningUserMessage = true;
+          } else if (ListeningUserMessage) {
+            addMessageToChat("user", transcript);
+
+	    flaskAjax(transcript);
+          } */
+	  flaskAjax(transcript);
+        }
+      }
+    };
+
+    recognition.start();
+
+    // 상시대기 STT 종료
+    $("#stopChromeSTT").click(function () {
+      recognition.stop();
+      ListeningUserMessage = false;
+      console.log("상시 대기 모드 종료");
+    });
+  } else {
+    console.error("Browser does not support webkitSpeechRecognition.");
+  }
+});
+
+// 챗봇, 유저, 선택지 출력
+function addMessageToChat(sender, message) {
+  const messageDiv = $("<div>").addClass(sender).text(message);
+  $(".chatdisplayArea-messageDiv").append(messageDiv);
+  const messageArea = document.querySelector(
+    ".main .middlearea .chatdisplayArea .chatdisplayArea-messageDiv"
+  );
+  if (sender == "bot") {
+    chromeTTS(message);
+  }
+  messageArea.scrollTop = messageArea.scrollHeight;
+}
+// 크롬 TTS
+function chromeTTS(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(utterance);
+}
+
+
+// 챗봇 응답
+function flaskAjax(transcript) {
+    console.log("Flask 앱 연동");
     $.ajax({
       url: newURL,
       method: "POST",
       contentType: "application/json",
-      data: JSON.stringify({ message: userMessage }),
+      data: JSON.stringify({ message: transcript }),
       success: function (data) {
-        addMessageToChat("user", `${userMessage}`);
+        addMessageToChat("user", `${transcript}`);
 
         // 딕셔너리 형태의 응답 중, data가 message가 있다면
         if (data.message) {
@@ -74,85 +154,4 @@ $(document).ready(function () {
         }
       },
     });
-  });
-
-  // 음성으로 입력할 때 챗봇
-  if ("webkitSpeechRecognition" in window) {
-    const recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-
-    let ListeningUserMessage = false;
-
-    recognition.onresult = function (event) {
-      console.log("음성 대기 중 ...");
-      for (let i = event.resultIndex; i < event.results.length; ++i) {
-        if (event.results[i].isFinal) {
-          const transcript = event.results[i][0].transcript.trim();
-          console.log(transcript);
-
-          if (transcript.includes("키오스키야") && !ListeningUserMessage) {
-            document.querySelector(".chatArea").style.display = "none";
-            document.querySelector(".chatdisplayArea").style.display = "block"; // jQuery 왜안됨
-
-            // 메시지를 chatbox에 추가
-            addMessageToChat(
-              "bot",
-              "어서오세요! 주문을 도와드리는 키오스키입니다^_^"
-            );
-            addMessageToChat("selector", "1. 메뉴 검색해줘");
-            addMessageToChat("selector", "2. 추천메뉴 알려줘");
-            addMessageToChat("selector", "3. 취향대로 추천해줘");
-            addMessageToChat("selector", "4. 주문 도와줘");
-            addMessageToChat("selector", "5. 직원 호출해 줘");
-
-            ListeningUserMessage = true;
-          } else if (ListeningUserMessage) {
-            addMessageToChat("user", transcript);
-
-	    // flask 챗봇 응답
-            $.ajax({
-              url: URL,
-              method: "POST",
-              contentType: "application/json",
-              data: JSON.stringify({ message: transcript }),
-              success: function (data) {
-                addMessageToChat("bot", `${data.response}`);
-              },
-            });
-          }
-        }
-      }
-    };
-
-    recognition.start();
-
-    // 상시대기 STT 종료
-    $("#stopChromeSTT").click(function () {
-      recognition.stop();
-      ListeningUserMessage = false;
-      console.log("상시 대기 모드 종료");
-    });
-  } else {
-    console.error("Browser does not support webkitSpeechRecognition.");
-  }
-});
-
-// 챗봇, 유저, 선택지 출력
-function addMessageToChat(sender, message) {
-  const messageDiv = $("<div>").addClass(sender).text(message);
-  $(".chatdisplayArea-messageDiv").append(messageDiv);
-  const messageArea = document.querySelector(
-    ".main .middlearea .chatdisplayArea .chatdisplayArea-messageDiv"
-  );
-  if (sender == "bot") {
-    chromeTTS(message);
-  }
-  messageArea.scrollTop = messageArea.scrollHeight;
 }
-// 크롬 TTS
-function chromeTTS(text) {
-  const utterance = new SpeechSynthesisUtterance(text);
-  window.speechSynthesis.speak(utterance);
-}
-
